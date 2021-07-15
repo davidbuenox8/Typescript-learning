bdf35b5d883b0c745bb23aa200d3aa4a9a427801
David Bueno <davidbuenox8@gmail.com>
2021-07-15 15:47:13 +0200

    sort project finished

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,24 +1,19 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-/* const numbersCollection = new NumbersCollection([4, 2, 0, -6, 3]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(numbersCollection.data); */
+const numbersCollection = new NumbersCollection([4, 2, 0, -6, 3]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
-/* const charactersCollection = new CharactersCollection('Hi High!');
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data); */
+const charactersCollection = new CharactersCollection('Hi High!');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
 linkedList.add(2);
 linkedList.add(19);
 linkedList.add(467);
-
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
