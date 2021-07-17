1256c494c573c60b277125856f7603b93b5cfc6e
David Bueno <davidbuenox8@gmail.com>
2021-07-17 12:23:12 +0200

    generics lesson finished

--- fetchjson/features/generics.ts
+++ fetchjson/features/generics.ts
@@ -19,4 +19,49 @@ class ArrayOfAnyting<T> {
   }
 }
 
-new ArrayOfAnyting<string>(['a', 'b', 'c']);
+const arrofstring = new ArrayOfAnyting(['a', 'b', 'c']);
+const arrayofnum = new ArrayOfAnyting([1, 'h', 3]);
+
+//Example of generics with functions
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+//Both together:
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+//Generic Constraints
+class Car {
+  print(): void {
+    console.log('I am a car');
+  }
+}
+
+class House {
+  print(): void {
+    console.log('I am a house');
+  }
+}
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+printHousesOrCars<Car>([new Car(), new Car(), new Car()]);
