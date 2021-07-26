0b22ecaf16d9b10eae0a488fece520bf9bf6d132
David Bueno <davidbuenox8@gmail.com>
2021-07-26 18:11:48 +0200

    Attributes.ts added

--- web/src/models/Sync.ts
+++ web/src/models/Sync.ts
@@ -1,13 +1,14 @@
 import axios, { AxiosPromise } from 'axios';
-import { UserProps } from './User';
 
-//http://localhost:3000/users
-export class Sync {
+interface HasId {
+  id?: number;
+}
+export class Sync<T extends HasId> {
   constructor(public rootUrl: string) {}
   fetch(id: number): AxiosPromise {
     return axios.get(`${this.rootUrl}/${id}`);
   }
-  save(data: UserProps): void {
+  save(data: T): void {
     const { id } = data;
     if (id) {
       axios.put(`${this.rootUrl}/${id}`, data);
