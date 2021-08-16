de6a6e3de2232072f6f8eb0928de794e4dce5c8b
David Bueno <davidbuenox8@gmail.com>
2021-08-16 19:05:02 +0200

    routes.ts added

--- server/src/controllers/LoginControllers.ts
+++ server/src/controllers/LoginControllers.ts
@@ -1,1 +1,21 @@
-class LoginController {}
+import { Router, Request, Response, NextFunction } from 'express';
+import { get } from './decorators/routes';
+
+@controller('/')
+class LoginController {
+  @get('/login')
+  getLogin(req: Request, res: Response): void {
+    res.send(`
+  <form method="POST">
+  <div>
+  <label>Email</label>
+  <input name="email"/>
+  </div>
+  <div>
+  <label>Password</label>
+  <input name="password" type="password"/>
+  </div>
+  <button>Submit</button>
+  </form>`);
+  }
+}
