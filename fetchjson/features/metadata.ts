0688a094fe850bee7e7b2822ce57730399a438c7
David Bueno <davidbuenox8@gmail.com>
2021-08-16 18:20:28 +0200

    metadata example finished

--- fetchjson/features/metadata.ts
+++ fetchjson/features/metadata.ts
@@ -1,9 +1,34 @@
 import 'reflect-metadata';
 
-const plane = {
+/* const plane = {
   color: 'red',
 };
 
-Reflect.defineMetadata('note', 'hi there', plane);
+Reflect.defineMetadata('note', 'hi there', plane, 'color');
+const note = Reflect.getMetadata('note', plane, 'color');
+console.log(note);
+ */
+@controller
+class Plane {
+  color: string = 'red';
 
-console.log(plane);
+  @get('/login')
+  fly(): void {
+    console.log('fly fly fly free');
+  }
+}
+
+function get(path: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata('path', path, target, key);
+  };
+}
+
+function controller(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const path = Reflect.getMetadata('path', target.prototype, key);
+    const middleware = Reflect.getMetadata('middleware', target.prototype, key);
+    //Route
+    //router.get(path, middleware, target.prototype[key]);
+  }
+}
