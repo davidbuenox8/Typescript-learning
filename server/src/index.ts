248dba9d297946e49b5fdd1add0fd181e0670d77
David Bueno <davidbuenox8@gmail.com>
2021-08-16 21:02:34 +0200

    methods.ts added

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,14 @@ import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
-
+import { AppRouter } from './AppRouter';
+import './controllers/LoginControllers';
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['dsakf'] }));
 app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
